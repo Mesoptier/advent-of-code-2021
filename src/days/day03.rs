d547c84099fea3da0c37091ec0fb6a1470cc442b
Koen Klaren <koen.klaren@gmail.com>
2021-12-18 21:24:15 +0100

    Improve day 3 using bitvec

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,86 +1,61 @@
+use bitvec::prelude::*;
+
 #[aoc_generator(day3)]
-pub fn input_generator(input: &str) -> Vec<String> {
+pub fn input_generator(input: &str) -> Vec<BitVec<Msb0>> {
     input
         .lines()
-        .map(|s| s.to_string())
+        .map(|s| BitVec::from_iter(s.chars().map(|c| c == '1')))
         .collect()
 }
 
 #[aoc(day3, part1)]
-pub fn solve_part1(input: &[String]) -> u32 {
-    let number_size = input[0].len();
+pub fn solve_part1(input: &[BitVec<Msb0>]) -> u32 {
+    let num_bits = input[0].len();
 
-    let mut total_count = 0;
-    let mut one_counts = vec![0; number_size];
+    let total_count = input.len();
 
-    for number_string in input {
-        total_count += 1;
-
-        for (i, c) in number_string.chars().enumerate() {
-            match c {
-                '1' => { one_counts[i] += 1; }
-                _ => {}
-            }
+    // Count '1' frequencies
+    let mut one_frequencies = vec![0; num_bits];
+    for n in input {
+        for i in n.iter_ones() {
+            one_frequencies[i] += 1;
         }
     }
 
-    let mut gamma_str: String = "".to_string();
-    let mut epsilon_str: String = "".to_string();
-    for i in 0..number_size {
-        if one_counts[i] >= total_count - one_counts[i] {
-            gamma_str = format!("{}1", gamma_str);
-            epsilon_str = format!("{}0", epsilon_str);
-        } else {
-            gamma_str = format!("{}0", gamma_str);
-            epsilon_str = format!("{}1", epsilon_str);
-        }
-    }
+    let gamma: BitVec<Msb0> = BitVec::from_iter(
+        one_frequencies.into_iter()
+            .map(|one_frequency| one_frequency >= total_count / 2)
+    );
+    let epsilon = !gamma.clone();
 
-    let gamma = u32::from_str_radix(gamma_str.as_str(), 2).unwrap();
-    let epsilon = u32::from_str_radix(epsilon_str.as_str(), 2).unwrap();
+    let gamma = gamma.load::<u32>();
+    let epsilon = epsilon.load::<u32>();
 
     gamma * epsilon
 }
 
 #[aoc(day3, part2)]
-pub fn solve_part2(input: &[String]) -> u32 {
+pub fn solve_part2(input: &[BitVec<Msb0>]) -> u32 {
     let oxygen_generator_rating = search_part2_number(Vec::from(input), true);
     let co2_scrubber_rating = search_part2_number(Vec::from(input), false);
     oxygen_generator_rating * co2_scrubber_rating
 }
 
-fn search_part2_number(mut numbers: Vec<String>, keep_most_common: bool) -> u32 {
-    let mut pos = 0;
+fn search_part2_number(mut numbers: Vec<BitVec<Msb0>>, keep_most_common_bit: bool) -> u32 {
+    let mut bit_pos = 0;
     while numbers.len() != 1 {
-        let filter_char = if most_common_bit_in_position(&numbers, pos) == keep_most_common {
-            '1'
-        } else {
-            '0'
-        };
+        let one_frequency = numbers.iter()
+            .filter(|n| n[bit_pos])
+            .count();
+        let zero_frequency = numbers.len() - one_frequency;
+        let filter_bit = (one_frequency >= zero_frequency) == keep_most_common_bit;
 
         numbers = numbers.into_iter()
-            .filter(|number| {
-                let c = number.as_bytes()[pos] as char;
-                c == filter_char
-            })
+            .filter(|n| n[bit_pos] == filter_bit)
             .collect();
 
-        pos += 1;
+        bit_pos += 1;
     }
 
-    u32::from_str_radix(numbers[0].as_str(), 2).unwrap()
-}
-
-fn most_common_bit_in_position(numbers: &Vec<String>, pos: usize) -> bool {
-    let total_count = numbers.len();
-    let mut one_count = 0;
-
-    for number in numbers {
-        let c = number.as_bytes()[pos] as char;
-        if c == '1' {
-            one_count += 1;
-        }
-    }
-
-    one_count >= total_count - one_count
+    numbers[0].load::<u32>()
 }
