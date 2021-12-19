8051b1f6e4cb8d2424911ff3e2a46cd3c390cbd9
Koen Klaren <koen.klaren@gmail.com>
2021-12-19 13:56:47 +0100

    Use the correct 24 orientations, instead of 48 including flips

--- src/days/day19.rs
+++ src/days/day19.rs
@@ -9,6 +9,52 @@ use nom::IResult;
 use nom::multi::{count, separated_list1};
 use nom::sequence::{delimited, terminated, tuple};
 
+static ROTATION_MATRICES: [[[i32; 3]; 3]; 24] = [
+    [[1, 0, 0], [0, 1, 0], [0, 0, 1]],
+    [[1, 0, 0], [0, 0, 1], [0, -1, 0]],
+    [[1, 0, 0], [0, -1, 0], [0, 0, -1]],
+    [[1, 0, 0], [0, 0, -1], [0, 1, 0]],
+    [[0, 1, 0], [0, 0, 1], [1, 0, 0]],
+    [[0, 1, 0], [1, 0, 0], [0, 0, -1]],
+    [[0, 1, 0], [0, 0, -1], [-1, 0, 0]],
+    [[0, 1, 0], [-1, 0, 0], [0, 0, 1]],
+    [[0, 0, 1], [1, 0, 0], [0, 1, 0]],
+    [[0, 0, 1], [0, 1, 0], [-1, 0, 0]],
+    [[0, 0, 1], [-1, 0, 0], [0, -1, 0]],
+    [[0, 0, 1], [0, -1, 0], [1, 0, 0]],
+    [[-1, 0, 0], [0, -1, 0], [0, 0, 1]],
+    [[-1, 0, 0], [0, 0, 1], [0, 1, 0]],
+    [[-1, 0, 0], [0, 1, 0], [0, 0, -1]],
+    [[-1, 0, 0], [0, 0, -1], [0, -1, 0]],
+    [[0, -1, 0], [0, 0, -1], [1, 0, 0]],
+    [[0, -1, 0], [1, 0, 0], [0, 0, 1]],
+    [[0, -1, 0], [0, 0, 1], [-1, 0, 0]],
+    [[0, -1, 0], [-1, 0, 0], [0, 0, -1]],
+    [[0, 0, -1], [-1, 0, 0], [0, 1, 0]],
+    [[0, 0, -1], [0, 1, 0], [1, 0, 0]],
+    [[0, 0, -1], [1, 0, 0], [0, -1, 0]],
+    [[0, 0, -1], [0, -1, 0], [-1, 0, 0]],
+];
+
+fn multiply_vec3_mat3(v: &[i32; 3], m: &[[i32; 3]; 3]) -> [i32; 3] {
+    [
+        v[0] * m[0][0] + v[1] * m[1][0] + v[2] * m[2][0],
+        v[0] * m[0][1] + v[1] * m[1][1] + v[2] * m[2][1],
+        v[0] * m[0][2] + v[1] * m[1][2] + v[2] * m[2][2],
+    ]
+}
+
+fn orientations(points: &Vec<[i32; 3]>) -> impl Iterator<Item=Vec<[i32; 3]>> + '_ {
+    ROTATION_MATRICES
+        .iter()
+        .map(|m| {
+            points
+                .iter()
+                .map(|p| multiply_vec3_mat3(p, m))
+                .collect()
+        })
+}
+
 #[aoc_generator(day19)]
 pub fn input_generator(input: &str) -> Vec<Vec<[i32; 3]>> {
     fn parse_input(input: &str) -> IResult<&str, Vec<Vec<[i32; 3]>>> {
@@ -35,7 +81,7 @@ pub fn input_generator(input: &str) -> Vec<Vec<[i32; 3]>> {
                 terminated(parse_signed_int, tag(",")),
                 parse_signed_int,
             )),
-            |(x, y, z)| [x, y, z]
+            |(x, y, z)| [x, y, z],
         )(input)
     }
 
@@ -92,48 +138,38 @@ pub fn solve_part2(input: &Vec<Vec<[i32; 3]>>) -> i32 {
         .tuple_combinations::<(_, _)>()
         .map(|(s1, s2)| {
             (s1[0] - s2[0]).abs()
-            + (s1[1] - s2[1]).abs()
-            + (s1[2] - s2[2]).abs()
+                + (s1[1] - s2[1]).abs()
+                + (s1[2] - s2[2]).abs()
         })
         .max()
         .unwrap()
 }
 
 fn find_match(beacons: &BTreeSet<[i32; 3]>, report: &Vec<[i32; 3]>) -> Option<([i32; 3], Vec<[i32; 3]>)> {
-    for pinned1 in beacons {
-        for pinned2 in report {
-            for i_xyz in [0, 1, 2].into_iter().permutations(3) {
-                let (ix, iy, iz) = match i_xyz[0..3] {
-                    [ix, iy, iz] => (ix, iy, iz),
-                    _ => unreachable!(),
-                };
-                for sx in [-1, 1] {
-                    for sy in [-1, 1] {
-                        for sz in [-1, 1] {
-                            // Translation
-                            let dx = pinned1[0] - pinned2[ix] * sx;
-                            let dy = pinned1[1] - pinned2[iy] * sy;
-                            let dz = pinned1[2] - pinned2[iz] * sz;
+    for rotated_report in orientations(report) {
+        for pinned1 in beacons {
+            for pinned2 in &rotated_report {
+                let dx = pinned1[0] - pinned2[0];
+                let dy = pinned1[1] - pinned2[1];
+                let dz = pinned1[2] - pinned2[2];
 
-                            let transformed_report = report.iter()
-                                .map(|beacon| [
-                                    beacon[ix] * sx + dx,
-                                    beacon[iy] * sy + dy,
-                                    beacon[iz] * sz + dz,
-                                ])
-                                .collect::<Vec<_>>();
+                let transformed_report = rotated_report
+                    .iter()
+                    .map(|p| [
+                        p[0] + dx,
+                        p[1] + dy,
+                        p[2] + dz,
+                    ])
+                    .collect::<Vec<_>>();
 
-                            let mut num_matches = 0;
-                            for transformed_beacon in &transformed_report {
-                                if beacons.contains(transformed_beacon) {
-                                    num_matches += 1;
-                                }
+                let mut num_matches = 0;
+                for transformed_beacon in &transformed_report {
+                    if beacons.contains(transformed_beacon) {
+                        num_matches += 1;
+                    }
 
-                                if num_matches >= 12 {
-                                    return Some(([dx, dy, dz], transformed_report));
-                                }
-                            }
-                        }
+                    if num_matches >= 12 {
+                        return Some(([dx, dy, dz], transformed_report));
                     }
                 }
             }
