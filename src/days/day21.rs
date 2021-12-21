c702058e79fec4c979e5af33dab8e008fcb0cda5
Koen Klaren <koen.klaren@gmail.com>
2021-12-21 21:00:24 +0100

    Dynamic programming!

--- src/days/day21.rs
+++ src/days/day21.rs
@@ -1,8 +1,5 @@
-use std::cmp::Ordering;
-use std::collections::BinaryHeap;
 use std::str::FromStr;
 
-use hashbrown::HashMap;
 use nom::bytes::complete::tag;
 use nom::character::complete::{digit1, newline};
 use nom::combinator::map_res;
@@ -59,74 +56,6 @@ pub fn solve_part1(input: &(usize, usize)) -> usize {
     die_rolls * loser_score
 }
 
-#[derive(Copy, Clone, Hash, Eq, PartialEq, Ord, PartialOrd, Debug)]
-enum Player {
-    Player1,
-    Player2,
-}
-
-#[derive(Copy, Clone, Hash, Eq, PartialEq, Debug)]
-struct State {
-    current_player: Player,
-    positions: (usize, usize),
-    scores: (usize, usize),
-}
-
-impl PartialOrd<Self> for State {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for State {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.min_score().cmp(&other.min_score()).reverse()
-            .then(self.max_score().cmp(&other.max_score()).reverse())
-    }
-}
-
-impl State {
-    fn other_player(&self) -> Player {
-        match self.current_player {
-            Player::Player1 => Player::Player2,
-            Player::Player2 => Player::Player1,
-        }
-    }
-
-    fn is_game_over(&self) -> bool {
-        let max_score = 21;
-        self.scores.0 == max_score || self.scores.1 == max_score
-    }
-
-    fn min_score(&self) -> usize {
-        self.scores.0.min(self.scores.1)
-    }
-
-    fn max_score(&self) -> usize {
-        self.scores.0.max(self.scores.1)
-    }
-
-    fn take_turn(&self, die_total: usize) -> State {
-        let num_spaces = 10;
-        let max_score = 21;
-
-        let new_positions = match self.current_player {
-            Player::Player1 => ((self.positions.0 + die_total) % num_spaces, self.positions.1),
-            Player::Player2 => (self.positions.0, (self.positions.1 + die_total) % num_spaces),
-        };
-        let new_scores = match self.current_player {
-            Player::Player1 => ((self.scores.0 + new_positions.0 + 1).min(max_score), self.scores.1),
-            Player::Player2 => (self.scores.0, (self.scores.1 + new_positions.1 + 1).min(max_score)),
-        };
-
-        State {
-            current_player: self.other_player(),
-            positions: new_positions,
-            scores: new_scores,
-        }
-    }
-}
-
 #[aoc(day21, part2)]
 pub fn solve_part2(input: &(usize, usize)) -> usize {
     let dice_rolls = [
@@ -139,52 +68,49 @@ pub fn solve_part2(input: &(usize, usize)) -> usize {
         (9, 1),
     ];
 
-    // Maps state to the number of states it can be reached from
-    let mut states_count: HashMap<State, usize> = HashMap::new();
+    let mut dp = [[[[[0; 2]; 10]; 10]; 21]; 21];
 
-    let mut q = BinaryHeap::new();
+    dp[0][0][input.0 - 1][input.1 - 1][0] = 1;
 
-    let initial_state = State {
-        current_player: Player::Player1,
-        positions: (input.0 - 1, input.1 - 1),
-        scores: (0, 0),
-    };
-    q.push(initial_state);
-    states_count.insert(initial_state, 1);
+    let mut p1_wins = 0;
+    let mut p2_wins = 0;
 
-    while let Some(state) = q.pop() {
-        let parent_count = states_count[&state];
+    for p1_score in 0..21 {
+        for p2_score in 0..21 {
+            for p1_pos in 0..10 {
+                for p2_pos in 0..10 {
+                    for current_player in 0..2 {
+                        let parent_count = dp[p1_score][p2_score][p1_pos][p2_pos][current_player];
+                        if parent_count == 0 {
+                            continue;
+                        }
 
-        for (die_total, universe_count) in dice_rolls {
-            let new_state = state.take_turn(die_total);
+                        for (die_total, roll_count) in dice_rolls {
+                            let (p1_pos, p2_pos) = match current_player {
+                                0 => ((p1_pos + die_total) % 10, p2_pos),
+                                1 => (p1_pos, (p2_pos + die_total) % 10),
+                                _ => unreachable!(),
+                            };
+                            let (p1_score, p2_score) = match current_player {
+                                0 => (p1_score + p1_pos + 1, p2_score),
+                                1 => (p1_score, p2_score + p2_pos + 1),
+                                _ => unreachable!(),
+                            };
+                            let current_player = (current_player + 1) % 2;
 
-            let foo = states_count.contains_key(&new_state);
-
-            *states_count.entry(new_state).or_insert(0) += universe_count * parent_count;
-
-            if new_state.is_game_over() {
-                continue;
-            }
-
-            if !foo {
-                q.push(new_state);
-            }
-        }
-    }
-
-    println!("{}", states_count.len());
-
-    let mut p1_count = 0;
-    let mut p2_count = 0;
-
-    for (state, count) in states_count {
-        if state.is_game_over() {
-            match state.other_player() {
-                Player::Player1 => p1_count += count,
-                Player::Player2 => p2_count += count,
+                            if p1_score >= 21 {
+                                p1_wins += parent_count * roll_count;
+                            } else if p2_score >= 21 {
+                                p2_wins += parent_count * roll_count;
+                            } else {
+                                dp[p1_score][p2_score][p1_pos][p2_pos][current_player] += parent_count * roll_count;
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 
-    p1_count.max(p2_count)
+    p1_wins.max(p2_wins)
 }
