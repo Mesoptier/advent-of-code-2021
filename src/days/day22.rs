9c1505091d54c66f586f9f4cbdc8341e35ee8f91
Koen Klaren <koen.klaren@gmail.com>
2021-12-22 16:52:33 +0100

    Day 22, part 2

--- src/days/day22.rs
+++ src/days/day22.rs
@@ -1,7 +1,6 @@
-use std::ops::RangeInclusive;
 use std::str::FromStr;
 
-use hashbrown::HashSet;
+use itertools::Itertools;
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::{digit1, newline};
@@ -10,28 +9,145 @@ use nom::IResult;
 use nom::multi::separated_list1;
 use nom::sequence::{preceded, separated_pair, tuple};
 
+#[derive(Copy, Clone, Debug)]
+pub struct Cuboid {
+    x1: i32,
+    x2: i32,
+    y1: i32,
+    y2: i32,
+    z1: i32,
+    z2: i32,
+}
+
+impl Cuboid {
+    fn intersection(&self, other: &Cuboid) -> Option<Cuboid> {
+        let x1 = self.x1.max(other.x1);
+        let x2 = self.x2.min(other.x2);
+        let y1 = self.y1.max(other.y1);
+        let y2 = self.y2.min(other.y2);
+        let z1 = self.z1.max(other.z1);
+        let z2 = self.z2.min(other.z2);
+
+        if x1 <= x2 && y1 <= y2 && z1 <= z2 {
+            Some(Cuboid {
+                x1,
+                x2,
+                y1,
+                y2,
+                z1,
+                z2,
+            })
+        } else {
+            None
+        }
+    }
+
+    fn sub(&self, other: &Cuboid) -> Vec<Cuboid> {
+        let mut result = vec![];
+
+        if let Some(intersection) = self.intersection(other) {
+            // Split X
+            let remainder = *self;
+            if self.x1 < intersection.x1 {
+                result.push({
+                    let mut slice = remainder;
+                    slice.x2 = intersection.x1 - 1;
+                    slice
+                });
+            }
+            if intersection.x2 < self.x2 {
+                result.push({
+                    let mut slice = remainder;
+                    slice.x1 = intersection.x2 + 1;
+                    slice
+                });
+            }
+
+            // Split Y
+            let remainder = {
+                let mut slice = remainder;
+                slice.x1 = intersection.x1;
+                slice.x2 = intersection.x2;
+                slice
+            };
+            if self.y1 < intersection.y1 {
+                result.push({
+                    let mut slice = remainder;
+                    slice.y2 = intersection.y1 - 1;
+                    slice
+                });
+            }
+            if intersection.y2 < self.y2 {
+                result.push({
+                    let mut slice = remainder;
+                    slice.y1 = intersection.y2 + 1;
+                    slice
+                });
+            }
+
+            // Split Z
+            let remainder = {
+                let mut slice = remainder;
+                slice.y1 = intersection.y1;
+                slice.y2 = intersection.y2;
+                slice
+            };
+            if self.z1 < intersection.z1 {
+                result.push({
+                    let mut slice = remainder;
+                    slice.z2 = intersection.z1 - 1;
+                    slice
+                });
+            }
+            if intersection.z2 < self.z2 {
+                result.push({
+                    let mut slice = remainder;
+                    slice.z1 = intersection.z2 + 1;
+                    slice
+                });
+            }
+        } else {
+            result.push(*self);
+        }
+
+        result
+    }
+
+    fn size(&self) -> usize {
+        (1 + self.x2 - self.x1) as usize
+            * (1 + self.y2 - self.y1) as usize
+            * (1 + self.z2 - self.z1) as usize
+    }
+}
+
 #[aoc_generator(day22)]
-pub fn input_generator(input: &str) -> Vec<(bool, RangeInclusive<i32>, RangeInclusive<i32>, RangeInclusive<i32>)> {
-    fn parse_step(input: &str) -> IResult<&str, (bool, RangeInclusive<i32>, RangeInclusive<i32>, RangeInclusive<i32>)> {
+pub fn input_generator(input: &str) -> Vec<(bool, Cuboid)> {
+    fn parse_step(input: &str) -> IResult<&str, (bool, Cuboid)> {
         tuple((
             alt((
                 map(tag("on"), |_| true),
                 map(tag("off"), |_| false)
             )),
-            preceded(tag(" x="), parse_range),
-            preceded(tag(",y="), parse_range),
-            preceded(tag(",z="), parse_range),
+            map(tuple((
+                preceded(tag(" x="), parse_range),
+                preceded(tag(",y="), parse_range),
+                preceded(tag(",z="), parse_range),
+            )), |((x1, x2), (y1, y2), (z1, z2))| Cuboid {
+                x1,
+                x2,
+                y1,
+                y2,
+                z1,
+                z2,
+            })
         ))(input)
     }
 
-    fn parse_range(input: &str) -> IResult<&str, RangeInclusive<i32>> {
-        map(
-            separated_pair(
-                parse_signed_int,
-                tag(".."),
-                parse_signed_int,
-            ),
-            |(start, end)| start..=end,
+    fn parse_range(input: &str) -> IResult<&str, (i32, i32)> {
+        separated_pair(
+            parse_signed_int,
+            tag(".."),
+            parse_signed_int,
         )(input)
     }
 
@@ -48,27 +164,57 @@ pub fn input_generator(input: &str) -> Vec<(bool, RangeInclusive<i32>, RangeIncl
     )(input).unwrap().1
 }
 
-fn range_intersect(r1: RangeInclusive<i32>, r2: RangeInclusive<i32>) -> RangeInclusive<i32> {
-    (*r1.start().max(r2.start()))..=(*r1.end().min(r2.end()))
-}
+// #[aoc(day22, part1)]
+// pub fn solve_part1(input: &Vec<(bool, RangeInclusive<i32>, RangeInclusive<i32>, RangeInclusive<i32>)>) -> usize {
+//     let mut cubes: HashSet<(i32, i32, i32)> = HashSet::new();
+//
+//     for (state, x_range, y_range, z_range) in input {
+//         for x in range_intersect(x_range.clone(), -50..=50) {
+//             for y in range_intersect(y_range.clone(), -50..=50) {
+//                 for z in range_intersect(z_range.clone(), -50..=50) {
+//                     if *state {
+//                         cubes.insert((x, y, z));
+//                     } else {
+//                         cubes.remove(&(x, y, z));
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//
+//     cubes.len()
+// }
 
-#[aoc(day22, part1)]
-pub fn solve_part1(input: &Vec<(bool, RangeInclusive<i32>, RangeInclusive<i32>, RangeInclusive<i32>)>) -> usize {
-    let mut cubes: HashSet<(i32, i32, i32)> = HashSet::new();
 
-    for (state, x_range, y_range, z_range) in input {
-        for x in range_intersect(x_range.clone(), -50..=50) {
-            for y in range_intersect(y_range.clone(), -50..=50) {
-                for z in range_intersect(z_range.clone(), -50..=50) {
-                    if *state {
-                        cubes.insert((x, y, z));
-                    } else {
-                        cubes.remove(&(x, y, z));
-                    }
-                }
+#[aoc(day22, part2)]
+pub fn solve_part2(input: &Vec<(bool, Cuboid)>) -> usize {
+    let mut on_cuboids = vec![];
+
+    for (state, cuboid) in input {
+        if *state {
+            // State: on
+            let mut new_cuboids = vec![*cuboid];
+
+            for on_cuboid in &on_cuboids {
+                new_cuboids = new_cuboids
+                    .into_iter()
+                    .flat_map(|new_cuboid| {
+                        new_cuboid.sub(on_cuboid)
+                    })
+                    .collect_vec()
             }
+
+            on_cuboids.extend(new_cuboids.into_iter());
+        } else {
+            // State: off
+            on_cuboids = on_cuboids
+                .into_iter()
+                .flat_map(|on_cuboid| {
+                    on_cuboid.sub(cuboid)
+                })
+                .collect_vec()
         }
     }
 
-    cubes.len()
+    on_cuboids.into_iter().map(|c| c.size()).sum::<usize>()
 }
