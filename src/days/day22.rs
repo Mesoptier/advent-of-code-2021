ae8cbc4ae82d2f1353282ca51eb7087357e3efbe
Koen Klaren <koen.klaren@gmail.com>
2021-12-22 20:10:46 +0100

    k-d tree, y'all!

--- src/days/day22.rs
+++ src/days/day22.rs
@@ -1,3 +1,4 @@
+use std::slice::Iter;
 use std::str::FromStr;
 
 use itertools::Itertools;
@@ -20,6 +21,43 @@ pub struct Cuboid {
 }
 
 impl Cuboid {
+    fn lower(&self, axis: Axis) -> i32 {
+        match axis {
+            Axis::X => self.x1,
+            Axis::Y => self.y1,
+            Axis::Z => self.z1,
+        }
+    }
+    fn lower_mut(&mut self, axis: Axis) -> &mut i32 {
+        match axis {
+            Axis::X => &mut self.x1,
+            Axis::Y => &mut self.y1,
+            Axis::Z => &mut self.z1,
+        }
+    }
+
+    fn upper(&self, axis: Axis) -> i32 {
+        match axis {
+            Axis::X => self.x2,
+            Axis::Y => self.y2,
+            Axis::Z => self.z2,
+        }
+    }
+    fn upper_mut(&mut self, axis: Axis) -> &mut i32 {
+        match axis {
+            Axis::X => &mut self.x2,
+            Axis::Y => &mut self.y2,
+            Axis::Z => &mut self.z2,
+        }
+    }
+
+    fn contains(&self, other: &Cuboid) -> bool {
+        Axis::iter().all(|&axis| {
+            self.lower(axis) <= other.lower(axis)
+                && other.upper(axis) <= self.upper(axis)
+        })
+    }
+
     fn intersection(&self, other: &Cuboid) -> Option<Cuboid> {
         let x1 = self.x1.max(other.x1);
         let x2 = self.x2.min(other.x2);
@@ -42,81 +80,223 @@ impl Cuboid {
         }
     }
 
-    fn sub(&self, other: &Cuboid) -> Vec<Cuboid> {
-        let mut result = vec![];
+    fn size(&self) -> usize {
+        Axis::iter().map(|&axis| {
+            (1 + self.upper(axis) - self.lower(axis)) as usize
+        }).product::<usize>()
+    }
 
-        if let Some(intersection) = self.intersection(other) {
-            // Split X
-            let remainder = *self;
-            if self.x1 < intersection.x1 {
-                result.push({
-                    let mut slice = remainder;
-                    slice.x2 = intersection.x1 - 1;
-                    slice
-                });
-            }
-            if intersection.x2 < self.x2 {
-                result.push({
-                    let mut slice = remainder;
-                    slice.x1 = intersection.x2 + 1;
-                    slice
-                });
-            }
+    fn split(&self, split_axis: SplitAxis) -> (Option<Cuboid>, Option<Cuboid>) {
+        let (axis, c) = split_axis;
 
-            // Split Y
-            let remainder = {
-                let mut slice = remainder;
-                slice.x1 = intersection.x1;
-                slice.x2 = intersection.x2;
-                slice
-            };
-            if self.y1 < intersection.y1 {
-                result.push({
-                    let mut slice = remainder;
-                    slice.y2 = intersection.y1 - 1;
-                    slice
-                });
-            }
-            if intersection.y2 < self.y2 {
-                result.push({
-                    let mut slice = remainder;
-                    slice.y1 = intersection.y2 + 1;
-                    slice
-                });
-            }
-
-            // Split Z
-            let remainder = {
-                let mut slice = remainder;
-                slice.y1 = intersection.y1;
-                slice.y2 = intersection.y2;
-                slice
-            };
-            if self.z1 < intersection.z1 {
-                result.push({
-                    let mut slice = remainder;
-                    slice.z2 = intersection.z1 - 1;
-                    slice
-                });
-            }
-            if intersection.z2 < self.z2 {
-                result.push({
-                    let mut slice = remainder;
-                    slice.z1 = intersection.z2 + 1;
-                    slice
-                });
-            }
+        if self.upper(axis) < c {
+            (Some(*self), None)
+        } else if c <= self.lower(axis) {
+            (None, Some(*self))
         } else {
-            result.push(*self);
+            let mut left_cuboid = *self;
+            *left_cuboid.upper_mut(axis) = c - 1;
+            let mut right_cuboid = *self;
+            *right_cuboid.lower_mut(axis) = c;
+            (Some(left_cuboid), Some(right_cuboid))
         }
+    }
+}
 
-        result
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum Axis {
+    X,
+    Y,
+    Z,
+}
+
+impl Axis {
+    fn iter() -> Iter<'static, Axis> {
+        static AXES: [Axis; 3] = [Axis::X, Axis::Y, Axis::Z];
+        AXES.iter()
+    }
+}
+
+type SplitAxis = (Axis, i32);
+
+#[derive(Clone, Debug)]
+enum CuboidNode {
+    Nil,
+    Leaf(Cuboid),
+    Split {
+        split_axis: SplitAxis,
+        left: Box<CuboidNode>,
+        right: Box<CuboidNode>,
+    },
+}
+
+impl CuboidNode {
+    fn add(&mut self, cuboid: Cuboid) {
+        match self {
+            CuboidNode::Nil => {
+                *self = CuboidNode::Leaf(cuboid);
+            }
+            CuboidNode::Split { split_axis, left, right } => {
+                let (left_cuboid, right_cuboid) = cuboid.split(*split_axis);
+                if let Some(left_cuboid) = left_cuboid {
+                    left.add(left_cuboid);
+                }
+                if let Some(right_cuboid) = right_cuboid {
+                    right.add(right_cuboid);
+                }
+            }
+            CuboidNode::Leaf(leaf_cuboid) => {
+                // Handle cases where cuboids completely overlap
+                if leaf_cuboid.contains(&cuboid) {
+                    return;
+                }
+                if cuboid.contains(leaf_cuboid) {
+                    *leaf_cuboid = cuboid;
+                    return;
+                }
+
+                // Handle cases where cuboids are completely disjoint
+                for &axis in Axis::iter() {
+                    if cuboid.upper(axis) < leaf_cuboid.lower(axis) {
+                        let split_axis: SplitAxis = (axis, leaf_cuboid.lower(axis));
+                        *self = CuboidNode::Split {
+                            split_axis,
+                            left: CuboidNode::Leaf(cuboid).into(),
+                            right: CuboidNode::Leaf(*leaf_cuboid).into(),
+                        };
+                        return;
+                    }
+                    if leaf_cuboid.upper(axis) < cuboid.lower(axis) {
+                        let split_axis: SplitAxis = (axis, cuboid.lower(axis));
+                        *self = CuboidNode::Split {
+                            split_axis,
+                            left: CuboidNode::Leaf(*leaf_cuboid).into(),
+                            right: CuboidNode::Leaf(cuboid).into(),
+                        };
+                        return;
+                    }
+                }
+
+                // Handle cases where cuboids partially overlap (complete overlap is handled above)
+                for &axis in Axis::iter() {
+                    if let Some((split_axis, left_node, right_node)) = {
+                        if cuboid.lower(axis) < leaf_cuboid.lower(axis) {
+                            let split_axis: SplitAxis = (axis, leaf_cuboid.lower(axis));
+                            let (left_cuboid, right_cuboid) = cuboid.split(split_axis);
+
+                            let left_node = CuboidNode::Leaf(left_cuboid.unwrap());
+                            let mut right_node = CuboidNode::Leaf(*leaf_cuboid);
+                            right_node.add(right_cuboid.unwrap());
+
+                            Some((split_axis, left_node, right_node))
+                        } else if leaf_cuboid.upper(axis) < cuboid.upper(axis) {
+                            let split_axis: SplitAxis = (axis, leaf_cuboid.upper(axis) + 1);
+                            let (left_cuboid, right_cuboid) = cuboid.split(split_axis);
+
+                            let mut left_node = CuboidNode::Leaf(*leaf_cuboid);
+                            left_node.add(left_cuboid.unwrap());
+                            let right_node = CuboidNode::Leaf(right_cuboid.unwrap());
+
+                            Some((split_axis, left_node, right_node))
+                        } else {
+                            None
+                        }
+                    } {
+                        *self = CuboidNode::Split {
+                            split_axis,
+                            left: left_node.into(),
+                            right: right_node.into(),
+                        };
+                        return;
+                    }
+                }
+
+                unreachable!();
+            }
+        }
+    }
+
+    fn sub(&mut self, cuboid: Cuboid) {
+        match self {
+            CuboidNode::Nil => {
+                return;
+            }
+            CuboidNode::Split { split_axis, left, right } => {
+                // TODO: Do we really need to split the cuboid here? Since it's a subtracting cuboid,
+                //  the invariant won't be broken if this cuboid stays at its original size.
+                let (left_cuboid, right_cuboid) = cuboid.split(*split_axis);
+                if let Some(left_cuboid) = left_cuboid {
+                    left.sub(left_cuboid);
+                }
+                if let Some(right_cuboid) = right_cuboid {
+                    right.sub(right_cuboid);
+                }
+
+                // TODO: Simplify if left/right is Nil after calling .sub()
+            }
+            CuboidNode::Leaf(leaf_cuboid) => {
+                if cuboid.contains(leaf_cuboid) {
+                    *self = CuboidNode::Nil;
+                    return;
+                }
+
+                // Handle cases where cuboids are completely disjoint
+                for &axis in Axis::iter() {
+                    if cuboid.upper(axis) < leaf_cuboid.lower(axis) {
+                        return;
+                    }
+                    if leaf_cuboid.upper(axis) < cuboid.lower(axis) {
+                        return;
+                    }
+                }
+
+                // Handle cases where cuboids overlap
+                for &axis in Axis::iter() {
+                    if let Some((split_axis, left_node, right_node)) = {
+                        if cuboid.upper(axis) < leaf_cuboid.upper(axis) {
+                            let split_axis: SplitAxis = (axis, cuboid.upper(axis) + 1);
+                            let (left_cuboid, right_cuboid) = leaf_cuboid.split(split_axis);
+
+                            let mut left_node = CuboidNode::Leaf(left_cuboid.unwrap());
+                            left_node.sub(cuboid);
+                            let right_node = CuboidNode::Leaf(right_cuboid.unwrap());
+
+                            Some((split_axis, left_node, right_node))
+                        } else if leaf_cuboid.lower(axis) < cuboid.lower(axis) {
+                            let split_axis: SplitAxis = (axis, cuboid.lower(axis));
+                            let (left_cuboid, right_cuboid) = leaf_cuboid.split(split_axis);
+
+                            let left_node = CuboidNode::Leaf(left_cuboid.unwrap());
+                            let mut right_node = CuboidNode::Leaf(right_cuboid.unwrap());
+                            right_node.sub(cuboid);
+
+                            Some((split_axis, left_node, right_node))
+                        } else {
+                            None
+                        }
+                    } {
+                        *self = CuboidNode::Split {
+                            split_axis,
+                            left: left_node.into(),
+                            right: right_node.into(),
+                        };
+                        return;
+                    }
+                }
+
+                unreachable!();
+            }
+        }
     }
 
     fn size(&self) -> usize {
-        (1 + self.x2 - self.x1) as usize
-            * (1 + self.y2 - self.y1) as usize
-            * (1 + self.z2 - self.z1) as usize
+        match self {
+            CuboidNode::Nil => 0,
+            CuboidNode::Leaf(cuboid) => cuboid.size(),
+            CuboidNode::Split { left, right, .. } => {
+                left.size() + right.size()
+            }
+        }
     }
 }
 
@@ -165,19 +345,21 @@ pub fn input_generator(input: &str) -> Vec<(bool, Cuboid)> {
 }
 
 fn solve_both_parts(input: &Vec<(bool, Cuboid)>, part1: bool) -> usize {
-    let mut on_cuboids = vec![];
+    let mut tree = CuboidNode::Nil;
+
+    let initialization_area = Cuboid {
+        x1: -50,
+        x2: 50,
+        y1: -50,
+        y2: 50,
+        z1: -50,
+        z2: 50,
+    };
 
     for (state, mut cuboid) in input {
         // Part 1: intersect cuboid with intersection area, skip if intersection is empty
         if part1 {
-            if let Some(intersection) = cuboid.intersection(&Cuboid {
-                x1: -50,
-                x2: 50,
-                y1: -50,
-                y2: 50,
-                z1: -50,
-                z2: 50,
-            }) {
+            if let Some(intersection) = cuboid.intersection(&initialization_area) {
                 cuboid = intersection;
             } else {
                 continue;
@@ -185,31 +367,13 @@ fn solve_both_parts(input: &Vec<(bool, Cuboid)>, part1: bool) -> usize {
         }
 
         if *state {
-            // State: on
-            let mut new_cuboids = vec![cuboid];
-
-            for on_cuboid in &on_cuboids {
-                new_cuboids = new_cuboids
-                    .into_iter()
-                    .flat_map(|new_cuboid| {
-                        new_cuboid.sub(on_cuboid)
-                    })
-                    .collect_vec()
-            }
-
-            on_cuboids.extend(new_cuboids.into_iter());
+            tree.add(cuboid);
         } else {
-            // State: off
-            on_cuboids = on_cuboids
-                .into_iter()
-                .flat_map(|on_cuboid| {
-                    on_cuboid.sub(&cuboid)
-                })
-                .collect_vec()
+            tree.sub(cuboid);
         }
     }
 
-    on_cuboids.into_iter().map(|c| c.size()).sum::<usize>()
+    tree.size()
 }
 
 #[aoc(day22, part1)]
